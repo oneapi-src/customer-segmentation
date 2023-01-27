8609329811dfa7e05e46b1cb09d9c75f9a889bf0
porteratzo <omar.montoya.chavez@intel.com>
2023-01-27 09:31:51 -0800

    fix benchmarks

--- src/run_exp_stock.sh
+++ src/run_exp_stock.sh
@@ -3,7 +3,7 @@
 # Copyright (C) 2022 Intel Corporation
 # SPDX-License-Identifier: BSD-3-Clause
 
-for j in 1 2 4
+for j in 1 10 100
 do
 	for n in {1..3}
 	do
@@ -12,7 +12,7 @@ do
 	done
 done
 
-for j in 1 2 4
+for j in 1 10 15
 do
 	for n in {1..3}
 	do
